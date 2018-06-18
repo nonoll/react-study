1e46445d37527250dc5d820d0ef7e4ee9013c0fb
HyunKwan Roh <hkroh.nonoll@gmail.com>
2018-06-18 14:22:04 +0900

    [hkroh] react 01

---
+++ 01-Setup a Simple React App/react-app/src/index.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+    <App />,
+    window.document.getElementById('root')
+);
